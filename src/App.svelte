fbdd4a3c565318cd71c8e2783f99d3c890152fb5
Wolf Developer <lupo.andrea.93@gmail.com>
2021-02-28 16:20:27 +0100

    2-way binding on custom components

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,26 @@
 <script>
+    import CustomInput from "./CustomInput.svelte";
+    import Toggle from "./Toggle.svelte";
 
+    let val = 'Max';
+    let price = 0;
+    let selectedOption = 1;
+
+    $:console.log(val);
+    $:console.log(selectedOption);
+
+    function setValue(event) {
+        val = event.target.value;
+    }
 </script>
 
-<h1>Bindings & Forms</h1>
+<!-- <input type="text" value={val} on:input={setValue}/> -->
+
+<!-- 2-way-binding -->
+<!-- <input type="text" bind:value={val}> -->
+
+<!-- bind:val - referes to the CustomInput val prop
+    {val} - referes to the App val -->
+<CustomInput bind:val={val}/>
+<Toggle bind:chosenOption={selectedOption}/>
+
