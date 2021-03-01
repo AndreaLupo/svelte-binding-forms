52b74aaee0d84951b1c46849160db9195341e497
Wolf Developer <lupo.andrea.93@gmail.com>
2021-03-01 08:14:08 +0100

    bind:this on simple and custom inputs

--- src/App.svelte
+++ src/App.svelte
@@ -9,14 +9,28 @@
     let favColor = 'green';
     let singleFavColor = 'red';
 
+    // html elements
+    let usernameInput;
+    let someDiv;
+    let customInput;
+
     $:console.log(val);
     $:console.log(selectedOption);
     $:console.log(agreed);
     $:console.log(favColor);
     $:console.log(singleFavColor);
 
+    $:console.log(customInput);
     function setValue(event) {
         val = event.target.value;
+    }
+
+    function saveData() {
+        // console.log(document.querySelector('#username').value);
+        console.log(usernameInput.value);
+        console.dir(someDiv);
+
+        customInput.empty();
     }
 </script>
 
@@ -27,7 +41,7 @@
 
 <!-- bind:val - referes to the CustomInput val prop
     {val} - referes to the App val -->
-<CustomInput bind:val={val}/>
+<CustomInput bind:val={val} bind:this={customInput}/>
 <Toggle bind:chosenOption={selectedOption}/>
 
 <input type="number" value={price}
@@ -57,3 +71,9 @@
     <option value="red">Red</option>
     <option value="blue">Blue</option>
 </select>
+
+
+<input id="username" type="text" bind:this={usernameInput}>
+<button on:click={saveData}>Save</button>
+
+<div bind:this={someDiv}>Ok!</div>
